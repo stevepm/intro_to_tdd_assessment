0ac6c135ebfddb68af946ebf22a0c31f2689703a
stevepm <steven.magelowitz@gmail.com>
2014-03-21 09:17:40 -0600

    added a few more tests to make sure that a few examples were tested

--- spec/prime_spec.rb
+++ spec/prime_spec.rb
@@ -6,6 +6,8 @@ describe 'PrimeNumber' do
     is_prime = PrimeNumber.new
 
     expect(is_prime.prime_number?(3)).to eq(true)
+    expect(is_prime.prime_number?(5)).to eq(true)
+    expect(is_prime.prime_number?(7)).to eq(true)
 
   end
 
@@ -13,6 +15,8 @@ describe 'PrimeNumber' do
     is_not_prime = PrimeNumber.new
 
     expect(is_not_prime.prime_number?(6)).to eq(false)
+    expect(is_not_prime.prime_number?(10)).to eq(false)
+    expect(is_not_prime.prime_number?(15)).to eq(false)
 
   end
 
